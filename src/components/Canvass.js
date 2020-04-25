e9c171f734efe62cd9a3fe6d6db90a31620c6f9f
jmh1397 <jmh1397@gmail.com>
2020-04-25 14:23:00 +0200

    1

---
+++ src/components/Canvass.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Canvass extends Component {
+    render(){
+        return(
+            <div>
+                Hello There
+            </div>
+        );
+    }
+}
+
+export default (Canvass);
