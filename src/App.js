c1d759cdc8571b141640ab60714631159e5506dc
jmh1397 <jmh1397@gmail.com>
2020-04-29 07:41:16 +0200

    Removed reference to existing blocks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Left from './components/Left';
-import Right from './components/Right';
-import Center from './components/Center';
+//import Header from './components/Header';
+//import Footer from './components/Footer';
+//import Left from './components/Left';
+//import Right from './components/Right';
+//import Center from './components/Center';
 
 class App extends Component {
   render() {
     return (
             <div id="MainApp">
-                <Header />
 
-                <Left />
-                <Right />
-                <Center />
-                <Footer />
             </div>
     );
   }
