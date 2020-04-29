8d48bd5deb23a82761630a7728ebbe747967039f
jmh1397 <jmh1397@gmail.com>
2020-04-29 08:00:46 +0200

    Responsive Menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-//import Header from './components/Header';
-//import Footer from './components/Footer';
-//import Left from './components/Left';
-//import Right from './components/Right';
-//import Center from './components/Center';
+
+import Menu from './components/Menu';
 
 class App extends Component {
   render() {
     return (
-            <div id="MainApp">
-
-            </div>
+        <div id="MainApp">
+          <Menu />
+        </div>
     );
   }
 }
---
+++ src/components/Menu.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../App.css';
+class Menu extends Component {
+    render(){
+        return(
+
+            <div class="col-3 col-s-3 menu">
+              <ul>
+                <li>New Entity</li>
+                <li>-</li>
+                <li>-</li>
+                <li>-</li>
+              </ul>
+            </div>
+        )
+      }
+    }
+
+export default (Menu);
